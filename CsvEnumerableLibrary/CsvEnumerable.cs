247f5f56ab4697a37d786c37515279b4feb9da9a
Hanna.Harr <Hanna.Harr@WSA-043-71>
2021-03-23 17:00:47 +0300

    Implement class CsvEnumerable and added example

--- CsvEnumerableLibrary/CsvEnumerable.cs
+++ CsvEnumerableLibrary/CsvEnumerable.cs
@@ -7,8 +7,33 @@ using System.IO;
 
 namespace CsvEnumerableLibrary
 {
-    public class Class1
+    public class CsvEnumerable<T> : IEnumerable<T>
     {
-        
+        private StreamReader streamReader;
+        private CsvReader csvReader;
+
+        public CsvEnumerable(string path)
+        {
+            streamReader = new StreamReader(path);
+            csvReader = new CsvReader(streamReader, CultureInfo.InvariantCulture);
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            return csvReader.GetRecords<T>().GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        ~CsvEnumerable()
+        {
+            csvReader.Dispose();
+
+            streamReader.Close();
+            streamReader.Dispose();
+        }
     }
 }
