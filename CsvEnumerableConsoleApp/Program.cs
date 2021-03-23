ef98f2e294e6f1e39c0305911197049e48306e4f
Hanna.Harr <Hanna.Harr@WSA-043-71>
2021-03-23 16:56:28 +0300

    Add project files.

---
+++ CsvEnumerableConsoleApp/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using CsvEnumerableLibrary;
+using System.Globalization;
+using System.IO;
+using CsvHelper;
+using System.Collections.Generic;
+
+namespace CsvEnumerableConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello world!");
+        }
+    }
+}
---
+++ CsvEnumerableLibrary/CsvEnumerable.cs
@@ -0,0 +1,14 @@
+﻿using CsvHelper;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+
+namespace CsvEnumerableLibrary
+{
+    public class Class1
+    {
+        
+    }
+}
