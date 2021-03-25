166f670434397062db17da89d76c12eecad72c8d
HannaHarr <HannaHarr@users.noreply.github.com>
2021-03-25 20:36:10 +0300

    Database connection

---
+++ WorkWithDatabaseLibrary/Class1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Text;
+
+namespace WorkWithDatabaseLibrary
+{
+    public static class Class1
+    {
+        // строка подключения к бд
+        // Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=D:\ITechArt\programs\WriteToDatabase\WorkWithDatabaseLibrary\WorkDatabase.mdf;Integrated Security=True
+        private static readonly string dataSource = @"(LocalDB)\MSSQLLocalDB";
+        // private static readonly string databaseFilename = $@"{Environment.CurrentDirectory}\WorkDatabase.mdf";
+        private static readonly string databaseFilename = @"D:\ITechArt\programs\WriteToDatabase\WorkWithDatabaseLibrary\WorkDatabase.mdf";
+
+        public static SqlConnection GetDBConnection()
+        {
+            string connString = $@"Data Source={dataSource};AttachDbFilename={databaseFilename};Integrated Security=True";
+
+            SqlConnection conn = new SqlConnection(connString);
+
+            return conn;
+        }
+    }
+}
--- WriteToDatabaseConsoleApp/Program.cs
+++ WriteToDatabaseConsoleApp/Program.cs
@@ -1,9 +1,10 @@
 ﻿using System;
 using System.Globalization;
 using System.IO;
-using CsvHelper;
 using WorkWithDatabaseLibrary;
 using System.Collections.Generic;
+using CsvHelper;
+using System.Data.SqlClient;
 
 namespace WriteToDatabaseConsoleApp
 {
@@ -11,17 +12,34 @@ namespace WriteToDatabaseConsoleApp
     {
         static void Main(string[] args)
         {
-            var path = @$"{Environment.CurrentDirectory}/text.csv";
+            //var path = @$"{Environment.CurrentDirectory}/text.csv";
 
-            CreateRecords(path);
+            //CreateRecords(path);
 
-            using (var csvRecords = new CsvEnumerable<Record>(path)) 
+            //using (var csvRecords = new CsvEnumerable<Record>(path)) 
+            //{
+            //    foreach (var record in csvRecords)
+            //    {
+            //        Console.WriteLine($"Id = {record.Id}, Name = {record.Name}");
+            //    }
+            //}
+
+            Console.WriteLine("Getting Connection ...");
+            SqlConnection conn = Class1.GetDBConnection();
+
+            try
             {
-                foreach (var record in csvRecords)
-                {
-                    Console.WriteLine($"Id = {record.Id}, Name = {record.Name}");
-                }
+                Console.WriteLine("Openning Connection ...");
+
+                conn.Open();
+
+                Console.WriteLine("Connection successful!");
             }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error: " + e.Message);
+            }
+
         }
 
         private static void CreateRecords(string filePath)
